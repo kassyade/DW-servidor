d6583e30aed848c539ffad3df98fe9296de763c3
kassyade <benjamin.aviles.std@gmail.com>
2024-10-28 12:42:13 +0100

    x

---
+++ Login/login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+    <p>Usuario<input type="text" name="usuario"></p>
+    <p>Contraseña <input type="password" name="contraseña"></p>
+    </form>
+</body>
+</html>
