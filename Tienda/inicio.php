614cb8f93de08ccbd54a9a2e972177d7ea371de1
kassyade <benjamin.aviles.std@gmail.com>
2024-11-05 09:37:45 +0100

    x

--- Tienda/inicio.php
+++ Tienda/inicio.php
@@ -4,13 +4,29 @@
     <meta charset="UTF-8">
     <title>Bienvenido</title>
 </head>
+
+<?php 
+//Gestion de usuarios y contraseñas
+$nombreUsuario = $_POST['nombreUsuario'];
+$contraseñaUsuario = $_POST['contraseñaUsuario'];
+
+
+?>
 <body>
-    <h1>Bienvenido a la tienda.</h1>
-    <h2>Introduce tu nombre para continuar.</h2>
-    <form action="tienda.php" method="POST">
-        <p></p>
-        <input type="text" name="nombre" required>
-        <button type="submit">Enviar</button>
-    </form>
+    <h1><center><b>Inicio de sesión</b></center></h1>
+    <hr>
+    <center>
+    <form action="tienda.php" method="post">
+    <p>Usuario</p>
+    <input type="text" name="nombre">
+    <p>Contraseña</p>
+    <input type="password" name="contraseña">
+    <p></p>
+    <button type="submit">Enviar</button>
+
+    </form> 
+    <p>¿No tienes cuenta?,Registrate <a href="registro.php">aquí</a></p> 
+    
+    </center>
 </body>
 </html>
