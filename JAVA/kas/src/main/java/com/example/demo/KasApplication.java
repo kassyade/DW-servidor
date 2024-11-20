2877224eb5256becfeadbe0bcc1b5b71ff819e35
kassyade <benjamin.aviles.std@gmail.com>
2024-11-20 11:08:58 +0100

    x

--- JAVA/kas/src/main/java/com/example/demo/KasApplication.java
+++ JAVA/kas/src/main/java/com/example/demo/KasApplication.java
@@ -1,13 +1,19 @@
 package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class KasApplication {
+    public static void main(String[] args) {
+      SpringApplication.run(KasApplication.class, args);
+    }
+    @PostMapping("/hola")
+    public String hello(@RequestParam(value = "nombre", defaultValue = "World") String name) {
+      return String.format("HOLA %s!", name);
 
-	public static void main(String[] args) {
-		SpringApplication.run(KasApplication.class, args);
-	}
-
+    }
 }
