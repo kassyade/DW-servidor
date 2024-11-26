85e7f79828cb388a6bd1de8354a25269a37f8adc
kassyade <benjamin.aviles.std@gmail.com>
2024-11-26 19:07:25 +0100

    x

---
+++ SpringTools/guia2/src/main/java/com/example/guia2/Guia2Application.java
@@ -0,0 +1,13 @@
+package com.example.guia2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Guia2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Guia2Application.class, args);
+	}
+
+}
--- guia1/src/main/java/com/example/guia1/GreetingController.java
+++ guia1/src/main/java/com/example/guia1/GreetingController.java
@@ -3,9 +3,23 @@ package com.example.guia1;
 //1 AÑADIMOS EL CONTROLADOR
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller 
 public class GreetingController {
     
-    
+    @GetMapping("/saludo")//para que podamos capturar los datos usando la url del puerto 8080
+
+    public String greeting(
+        //queremos capturar un nombre (no obligatorio) 
+        @RequestParam (name = "nombre",required=false ,defaultValue="mundo")
+        //capturamos  el nombre y el modelo en el que se van a usar 
+        String name , Model model
+        ){
+            //pasamos los datos al modelo
+            model.addAttribute("nombre",name);
+            return "saludo";
+        }
 }
