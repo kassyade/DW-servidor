c96800ee54401a47c9a1d605ab706f13cd0cbef1
kassyade <benjamin.aviles.std@gmail.com>
2024-11-26 11:09:53 +0100

    x

---
+++ guia1/src/main/java/com/example/guia1/GreetingController.java
@@ -0,0 +1,10 @@
+package com.example.guia1;
+
+//1 AÑADIMOS EL CONTROLADOR
+
+import org.springframework.stereotype.Controller;
+
+@Controller 
+public class GreetingController {
+    
+}
---
+++ guia1/src/test/java/com/example/guia1/Guia1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guia1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Guia1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
