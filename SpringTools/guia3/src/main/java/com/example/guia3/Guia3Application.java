c07e37e7dd79ac5f619f285f7223dfca8ba2ee09
kassyade <benjamin.aviles.std@gmail.com>
2024-11-27 10:56:12 +0100

    x

---
+++ SpringTools/guia3/src/main/java/com/example/guia3/Guia3Application.java
@@ -0,0 +1,13 @@
+package com.example.guia3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Guia3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Guia3Application.class, args);
+	}
+
+}
