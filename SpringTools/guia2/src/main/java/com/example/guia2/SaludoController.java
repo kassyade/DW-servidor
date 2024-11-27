c0feed48cf4ed582d2db6f58727fa5a98116c02b
kassyade <benjamin.aviles.std@gmail.com>
2024-11-27 10:46:51 +0100

    x

--- SpringTools/guia2/src/main/java/com/example/guia2/SaludoController.java
+++ SpringTools/guia2/src/main/java/com/example/guia2/SaludoController.java
@@ -1,8 +1,25 @@
 package com.example.guia2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SaludoController {
-    
+
+
+    @GetMapping("/saludo")
+    public String FormularioSaludo(Model model){
+        model.addAttribute("saludo",new Saludo());
+        return "saludo";
+    }
+
+    @PostMapping("/saludo")
+    public String SaludoSubmit(@ModelAttribute Saludo saludo, Model model){
+        model.addAttribute("saludo", saludo);
+        return "resultado";
+    }
+
 }
