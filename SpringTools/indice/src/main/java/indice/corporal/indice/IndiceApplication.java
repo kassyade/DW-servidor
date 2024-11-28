b80e3c9a881ca5058c3c84bd0b4719a173bd1a33
kassyade <benjamin.aviles.std@gmail.com>
2024-11-28 12:04:44 +0100

    x

---
+++ SpringTools/indice/src/main/java/indice/corporal/indice/IndiceApplication.java
@@ -0,0 +1,13 @@
+package indice.corporal.indice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndiceApplication.class, args);
+	}
+
+}
---
+++ SpringTools/indice/src/test/java/indice/corporal/indice/IndiceApplicationTests.java
@@ -0,0 +1,13 @@
+package indice.corporal.indice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
