02a3c4ab1b5ad82faaeda64271a572dfad3d54e6
kassyade <benjamin.aviles.std@gmail.com>
2024-12-17 12:01:21 +0100

    proyecto quiz temrinado

--- SpringTools/quiz/src/main/java/quiz/id/quiz/Controlador.java
+++ SpringTools/quiz/src/main/java/quiz/id/quiz/Controlador.java
@@ -39,6 +39,29 @@ public class Controlador {
         return "pregunta3";
     }
 
+        @PostMapping("/pregunta4")
+    public String procesarPregunta4(@ModelAttribute Usuario usuario, @RequestParam("respuesta") int respuesta) {
+        usuario.agregarPuntuacion(respuesta);
+        return "pregunta4";
+    }
+            @PostMapping("/pregunta5")
+    public String procesarPregunta5(@ModelAttribute Usuario usuario, @RequestParam("respuesta") int respuesta) {
+        usuario.agregarPuntuacion(respuesta);
+        return "pregunta5";
+    }
+            @PostMapping("/pregunta6")
+    public String procesarPregunta6(@ModelAttribute Usuario usuario, @RequestParam("respuesta") int respuesta) {
+        usuario.agregarPuntuacion(respuesta);
+        return "pregunta6";
+    }
+            @PostMapping("/pregunta7")
+    public String procesarPregunta7(@ModelAttribute Usuario usuario, @RequestParam("respuesta") int respuesta) {
+        usuario.agregarPuntuacion(respuesta);
+        return "pregunta7";
+    }
+
+
+
     @PostMapping("/resultado")
     public String resultado(@ModelAttribute Usuario usuario, @RequestParam("respuesta") int respuesta, Model model) {
         usuario.agregarPuntuacion(respuesta);
@@ -48,16 +71,20 @@ public class Controlador {
 
         return "resultado";
     }
-
     private String calcularLenguaje(int puntuacionTotal) {
-        if (puntuacionTotal <= 4) {
-            return "JavaScript: dinámico y flexible para desarrollo web.";
+        if (puntuacionTotal <= 5) {
+            return "HTML/CSS: ideal para la creación de estructuras y estilos en sitios web.";
         } else if (puntuacionTotal <= 8) {
-            return "PHP: perfecto para la conexión con bases de datos y desarrollo web.";
+            return "JavaScript: dinámico y flexible para desarrollo web.";
         } else if (puntuacionTotal <= 12) {
+            return "PHP: perfecto para la conexión con bases de datos y desarrollo web.";
+        } else if (puntuacionTotal <= 16) {
             return "Java: robusto y excelente para aplicaciones concurrentes.";
+        } else if (puntuacionTotal <= 20) {
+            return "C#: versátil y excelente para aplicaciones de escritorio y videojuegos.";
         } else {
-            return "Python: ideal para análisis de datos y automatización.";
+            return "Python: ideal para análisis de datos, automatización e inteligencia artificial.";
         }
     }
+    
 }
