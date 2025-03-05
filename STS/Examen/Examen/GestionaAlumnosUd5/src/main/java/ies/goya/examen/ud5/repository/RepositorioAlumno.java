91cde79a659d55c0170972547587497eedf2f65e
kassyade <benjamin.aviles.std@gmail.com>
2025-03-05 06:39:43 +0100

    xs

--- STS/Examen/Examen/GestionaAlumnosUd5/src/main/java/ies/goya/examen/ud5/repository/RepositorioAlumno.java
+++ STS/Examen/Examen/GestionaAlumnosUd5/src/main/java/ies/goya/examen/ud5/repository/RepositorioAlumno.java
@@ -1,10 +1,13 @@
 package ies.goya.examen.ud5.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ies.goya.examen.ud5.model.Alumno;
 
+
 public interface RepositorioAlumno extends JpaRepository<Alumno,Long>{
-	
+	List<Alumno> findByNombre(String nombre); 
 	
 }
